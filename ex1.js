ebb79c9f93c2a435c4639dec63299bacae793882
יאנה בלום <blum@h-MacBook-Pro-sl-ynh.local>
2020-09-21 13:42:38 +0300

    finish exam

--- ex1.js
+++ ex1.js
@@ -25,4 +25,4 @@ if(bookName in books){
 
 }
 
-getBookInfo()
+// getBookInfo()
--- ex4.js
+++ ex4.js
@@ -1,9 +1,11 @@
 const addInputToPage = function(input){
+  for(let i=0;i<10;i++)
   $('#content').append(`<p>${input}</p>`)
 }
 $('#submit').on('click',function(){
   const name = $('#name').val()
   addInputToPage(name)
 })
+
 
 addInputToPage('shoobert')
